b597eecce040844211dfad940a1990394e11811f
atmoPunk <atmopunk@outlook.com>
2018-05-25 12:18:21 +0300

    added arrow key navigation

--- kilo.c
+++ kilo.c
@@ -15,9 +15,12 @@
 
 #define KILO_VERSION "0.0.1"
 
+enum editorKey { ARROW_LEFT = 1000, ARROW_RIGHT, ARROW_UP, ARROW_DOWN };
+
 /*** data ***/
 
 struct editorConfig {
+  int cx, cy;
   int screenrows;
   int screencols;
   struct termios orig_termios;
@@ -35,7 +38,7 @@ struct abuf {
 void die(const char *);
 void disableRawMode();
 void enableRawMode();
-char editorReadKey();
+int editorReadKey();
 int getCursorPosition(int *, int *);
 int getWindowSize(int *, int *);
 
@@ -43,6 +46,7 @@ void editorClearScreen();
 void editorDrawRows(struct abuf *);
 void editorRefreshScreen();
 
+void editorMoveCursor(int);
 void editorProcesssKeypress();
 
 /*** terminal ***/
@@ -79,7 +83,7 @@ void enableRawMode() {
   }
 }
 
-char editorReadKey() {
+int editorReadKey() {
   int nread;
   char c;
   while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
@@ -87,7 +91,37 @@ char editorReadKey() {
       die("read");
     }
   }
-  return c;
+
+  if (c == '\x1b') {
+    char seq[3];
+    if (read(STDIN_FILENO, &seq[0], 1) != 1) {
+      return '\x1b';
+    }
+    if (read(STDIN_FILENO, &seq[1], 1) != 1) {
+      return '\x1b';
+    }
+
+    if (seq[0] == '[') {
+      switch (seq[1]) {
+      case 'A':
+        return ARROW_UP;
+        break;
+      case 'B':
+        return ARROW_DOWN;
+        break;
+      case 'C':
+        return ARROW_RIGHT;
+        break;
+      case 'D':
+        return ARROW_LEFT;
+        break;
+      }
+    }
+
+    return '\x1b';
+  } else {
+    return c;
+  }
 }
 
 int getCursorPosition(int *rows, int *cols) {
@@ -200,7 +234,10 @@ void editorRefreshScreen() {
 
   editorDrawRows(&ab);
 
-  abAppend(&ab, "\x1b[H", 3);
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
+
   abAppend(&ab, "\x1b[?25h", 6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
@@ -209,13 +246,36 @@ void editorRefreshScreen() {
 
 /*** input ***/
 
+void editorMoveCursor(int key) {
+  switch (key) {
+  case ARROW_LEFT:
+    E.cx--;
+    break;
+  case ARROW_RIGHT:
+    E.cx++;
+    break;
+  case ARROW_UP:
+    E.cy--;
+    break;
+  case ARROW_DOWN:
+    E.cy++;
+  }
+}
+
 void editorProcessKeypress() {
-  char c = editorReadKey();
+  int c = editorReadKey();
 
   switch (c) {
   case CTRL_KEY('q'):
     editorClearScreen();
     exit(0);
+    break;
+
+  case ARROW_UP:
+  case ARROW_DOWN:
+  case ARROW_LEFT:
+  case ARROW_RIGHT:
+    editorMoveCursor(c);
     break;
   }
 }
@@ -223,6 +283,9 @@ void editorProcessKeypress() {
 /*** init ***/
 
 void initEditor() {
+  E.cx = 0;
+  E.cy = 0;
+
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) {
     die("getWindowSize");
   }
