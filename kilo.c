ac23d134a40e04a580c291afadeaa7e154ba4824
atmoPunk <atmopunk@outlook.com>
2018-05-27 20:04:25 +0300

    Added file modifiyng tracking

--- kilo.c
+++ kilo.c
@@ -59,6 +59,7 @@ struct editorConfig {
   int screencols;
   int numrows;
   erow *row;
+  int dirty;
   char *filename;
   char statusmsg[80];
   time_t statusmsg_time;
@@ -340,49 +341,51 @@ void editorAppendRow(char *s, size_t len) {
   editorUpdateRow(&E.row[at]);
 
   E.numrows++;
+  E.dirty++;
 }
 
 void editorRowInsertChar(erow *row, int at, int c) {
-    if(at < 0 || at > row->size) {
-        at = row->size;
-    }
-    row->chars = realloc(row->chars, row->size + 2);
-    memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
-    row->size++;
-    row->chars[at] = c;
-    editorUpdateRow(row);
+  if (at < 0 || at > row->size) {
+    at = row->size;
+  }
+  row->chars = realloc(row->chars, row->size + 2);
+  memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
+  row->size++;
+  row->chars[at] = c;
+  editorUpdateRow(row);
+  E.dirty++;
 }
 
 /*** editor operations ***/
 
 void editorInsertChar(int c) {
-    if(E.cy == E.numrows) {
-        editorAppendRow("", 0);
-    }
-    editorRowInsertChar(&E.row[E.cy], E.cx, c);
-    E.cx++;
+  if (E.cy == E.numrows) {
+    editorAppendRow("", 0);
+  }
+  editorRowInsertChar(&E.row[E.cy], E.cx, c);
+  E.cx++;
 }
 
 /*** file i/o ***/
 
 char *editorRowsToString(int *buflen) {
-    int totlen = 0;
-    int j;
-    for(j = 0; j < E.numrows; j++) {
-        totlen += E.row[j].size + 1;
-    }
-    *buflen = totlen;
+  int totlen = 0;
+  int j;
+  for (j = 0; j < E.numrows; j++) {
+    totlen += E.row[j].size + 1;
+  }
+  *buflen = totlen;
 
-    char *buf = malloc(totlen);
-    char *p = buf;
-    for(j = 0; j < E.numrows; j++) {
-        memcpy(p, E.row[j].chars, E.row[j].size);
-        p += E.row[j].size;
-        *p = '\n';
-        p++;
-    }
+  char *buf = malloc(totlen);
+  char *p = buf;
+  for (j = 0; j < E.numrows; j++) {
+    memcpy(p, E.row[j].chars, E.row[j].size);
+    p += E.row[j].size;
+    *p = '\n';
+    p++;
+  }
 
-    return buf;
+  return buf;
 }
 
 void editorOpen(char *filename) {
@@ -407,30 +410,32 @@ void editorOpen(char *filename) {
   }
   free(line);
   fclose(fp);
+  E.dirty = 0;
 }
 
 void editorSave() {
-    if(E.filename == NULL) {
+  if (E.filename == NULL) {
+    return;
+  }
+
+  int len;
+  char *buf = editorRowsToString(&len);
+
+  int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+  if (fd != -1) {
+    if (ftruncate(fd, len) != -1) {
+      if (write(fd, buf, len) == len) {
+        close(fd);
+        free(buf);
+        E.dirty = 0;
+        editorSetStatusMessage("%d bytes written to disk", len);
         return;
-    }
-
-    int len;
-    char *buf = editorRowsToString(&len);
-
-    int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
-    if(fd != -1) {
-      if(ftruncate(fd, len) != -1) {
-        if(write(fd, buf, len) == len) {
-          close(fd);
-          free(buf);
-          editorSetStatusMessage("%d bytes written to disk", len);
-          return;
-        }
       }
-      close(fd);
     }
-    free(buf);
-    editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
+    close(fd);
+  }
+  free(buf);
+  editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
 }
 
 /*** append buffer ***/
@@ -525,8 +530,9 @@ void editorDrawStatusBar(struct abuf *ab) {
   abAppend(ab, "\x1b[7m", 4);
   char status[80];
   char rstatus[80];
-  int len = snprintf(status, sizeof(status), "%.20s - %d lines",
-                     E.filename ? E.filename : "[No Name]", E.numrows);
+  int len = snprintf(status, sizeof(status), "%.20s - %d lines %s",
+                     E.filename ? E.filename : "[No Name]", E.numrows,
+                     E.dirty ? "(modified)" : "");
   int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
   if (len > E.screencols) {
     len = E.screencols;
@@ -646,8 +652,8 @@ void editorProcessKeypress() {
 
   switch (c) {
   case '\r':
-      /*  TODO */
-      break;
+    /*  TODO */
+    break;
 
   case CTRL_KEY('q'):
     editorClearScreen();
@@ -721,8 +727,9 @@ void initEditor() {
   E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
+  E.dirty = 0;
   E.filename = NULL;
-  E.statusmsg[0] - '\0';
+  E.statusmsg[0] = '\0';
   E.statusmsg_time = 0;
 
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) {
