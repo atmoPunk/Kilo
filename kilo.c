64d6a899a4d4687c506cfe6204a4ec8dd48fac5e
atmoPunk <atmopunk@outlook.com>
2018-05-27 23:01:06 +0300

    added newline insertion

--- kilo.c
+++ kilo.c
@@ -86,7 +86,7 @@ int getWindowSize(int *, int *);
 int editorRowRxToCx(erow *, int);
 int editorRowCxToRx(erow *, int);
 void editorUpdateRow(erow *);
-void editorAppendRow(char *, size_t);
+void editorInsertRow(int, char *, size_t);
 void editorFreeRow(erow *);
 void editorDelRow(int at);
 void editorRowInsertChar(erow *, int, int);
@@ -94,6 +94,7 @@ void editorRowAppendString(erow *, char *, size_t);
 void editorRowDelChar(erow *, int);
 
 void editorInsertChar(int);
+void editorInsertNewline();
 void editorDelChar();
 
 char *editorRowsToString(int *);
@@ -333,10 +334,14 @@ void editorUpdateRow(erow *row) {
   row->rsize = idx;
 }
 
-void editorAppendRow(char *s, size_t len) {
-  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+void editorInsertRow(int at, char *s, size_t len) {
+  if (at < 0 || at > E.numrows) {
+    return;
+  }
 
-  int at = E.numrows;
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+
   E.row[at].size = len;
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
@@ -398,10 +403,25 @@ void editorRowDelChar(erow *row, int at) {
 
 void editorInsertChar(int c) {
   if (E.cy == E.numrows) {
-    editorAppendRow("", 0);
+    editorInsertRow(E.numrows, "", 0);
   }
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
   E.cx++;
+}
+
+void editorInsertNewline() {
+  if (E.cx == 0) {
+    editorInsertRow(E.cy, "", 0);
+  } else {
+    erow *row = &E.row[E.cy];
+    editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+    row = &E.row[E.cy];
+    row->size = E.cx;
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+  }
+  E.cy++;
+  E.cx = 0;
 }
 
 void editorDelChar() {
@@ -464,7 +484,7 @@ void editorOpen(char *filename) {
            (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) {
       linelen--;
     }
-    editorAppendRow(line, linelen);
+    editorInsertRow(E.numrows, line, linelen);
   }
   free(line);
   fclose(fp);
@@ -712,7 +732,7 @@ void editorProcessKeypress() {
 
   switch (c) {
   case '\r':
-    /*  TODO */
+    editorInsertNewline();
     break;
 
   case CTRL_KEY('q'):
